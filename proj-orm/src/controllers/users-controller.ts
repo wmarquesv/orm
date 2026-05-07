644e4b489f26a1936919233a99b1e1920b4e753b
wmarquesv <walace.mv@hotmail.com>
2026-05-07 18:55:45 -0300

    introducao a orm

---
+++ proj-orm/src/controllers/users-controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+class UsersController {
+  async index(request: Request, response: Response) {
+    return response.json();
+  }
+
+  async create(request: Request, response: Response) {
+    return response.status(201).json();
+  }
+
+  async show(request: Request, response: Response) {
+    return response.json();
+  }
+}
+
+export { UsersController };
