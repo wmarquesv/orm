e3233e446671dd69e89b4941946f0a79d1a25b6e
wmarquesv <walace.mv@hotmail.com>
2026-05-08 01:19:41 -0300

    adicionando metodo create a requisicao das perguntas

--- proj-orm/src/controllers/questions-controller.ts
+++ proj-orm/src/controllers/questions-controller.ts
@@ -1,21 +1,28 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { prisma } from "@/prisma";
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json()
+    const { title, content, user_id } = request.body;
+
+    await prisma.questions.create({
+      data: { title, content, userId: user_id },
+    });
+
+    return response.status(201).json();
   }
 
   async update(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async remove(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 }
 
-export { QuestionsController }
+export { QuestionsController };
--- proj-orm/src/controllers/users-controller.ts
+++ proj-orm/src/controllers/users-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { prisma } from "@/prisma.js";
+import { prisma } from "@/prisma";
 
 class UsersController {
   async index(request: Request, response: Response) {
